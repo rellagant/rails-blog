87496621eaf348d0eb1520d656ef90e1f3c0f916
rellagant <barbara.cs@gmail.com>
2024-09-01 00:35:54 -0400

    fixed syntax in controller methods; generated comment model; ran migration:

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create 
-    @arcticle = Article.new(article_params)
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
