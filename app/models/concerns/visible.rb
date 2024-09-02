94d7a4d3fd9b84273f3e9b3074f7f40011fd7223
rellagant <barbara.cs@gmail.com>
2024-09-01 23:28:43 -0400

    added class method to Visible to display count of public articles/comments

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -3,10 +3,16 @@ module Visible
 
     VALID_STATUSES = ['public', 'private', 'archived']
 
-    invluded do
+    included do
         validates :status, inclusion: { in: VALID_STATUSES }
     end 
-    
+
+    class_methods do
+        def public_count
+            where(status: 'public').count
+        end
+    end
+
     def archived? 
         status == 'archived'
     end 
